71366bd2dc19c7af6446370f90b2c3b701b9ef66
Adam Gertzkin <101058145+gertzMan@users.noreply.github.com>
2023-03-30 10:10:22 +0300

    initial commit

---
+++ js/game.js
@@ -0,0 +1,185 @@
+'use strict';
+
+const EMPTY = '';
+const MINE = '💥';
+const FLAG = '🚩';
+
+var gBoard;
+var gLevel = { SIZE: 4, MINES: 2 };
+var gGame;
+function onInit() {
+  gGame = {
+    isOn: true, //boolean - true playing is allowed, false not allowed
+    shownCount: 0, // how many cells are shown
+    markedCount: 0, // how many cells are marked
+    secsPassed: 0, // how many seconds passed
+  };
+
+  gBoard = buildBoard();
+  preventRightClick();
+  deployMines();
+  setMinesNegsCount(gBoard);
+  renderBoard();
+}
+
+function buildBoard() {
+  const board = [];
+  for (var i = 0; i < gLevel.SIZE; i++) {
+    board[i] = [];
+    for (var j = 0; j < gLevel.SIZE; j++) {
+      board[i][j] = getInitialCell(i, j);
+    }
+  }
+
+  return board;
+}
+
+function getInitialCell(posI, posJ) {
+  return {
+    minesAroundCount: 0,
+    isShown: false,
+    isMine: false,
+    isMarked: false,
+  };
+}
+
+function deployMines() {
+  // for (var i = 0; i < gLevel.MINES; i++) {
+  //   const randI = getRandomInt(0, gLevel.SIZE);
+  //   const randJ = getRandomInt(0, gLevel.SIZE);
+  //   gBoard[randI][randJ].isMine = true;
+  // }
+  gBoard[2][1].isMine = true;
+  gBoard[2][2].isMine = true;
+}
+
+function renderBoard() {
+  var strHTML = '<table border="0"><tbody>';
+  for (var i = 0; i < gBoard.length; i++) {
+    strHTML += '<tr>\n';
+    for (var j = 0; j < gBoard.length; j++) {
+      var cellClass = getClassName(i, j) + ' ';
+      strHTML += `<td class="cell ${cellClass}"
+            oncontextmenu="onCellMarked(this,${i},${j})" onclick="onCellClicked(this,${i},${j})">`;
+      strHTML += '</td>\n';
+    }
+    strHTML += '</tr>\n';
+  }
+  strHTML += '</tbody></table>';
+  const elBoardContainer = document.querySelector('.board-container');
+  elBoardContainer.innerHTML = strHTML;
+}
+
+function renderCell(i, j, value) {
+  // select the elCell and set the value
+  const elCell = document.querySelector(`.cell-${i}-${j}`);
+  elCell.innerText = value;
+}
+
+function getClassName(i, j) {
+  var cellClass = `cell-${i}-${j}`;
+  return cellClass;
+}
+
+function setMinesNegsCount(board) {
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      gBoard[i][j].minesAroundCount = getMineNegsCount(i, j, board);
+    }
+  }
+}
+
+function getMineNegsCount(rowIdx, colIdx, board) {
+  var negsMineCount = 0;
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= board.length) continue;
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (i === rowIdx && j === colIdx) continue;
+      if (j < 0 || j >= board[i].length) continue;
+      if (board[i][j].isMine) negsMineCount++;
+    }
+  }
+  return negsMineCount;
+}
+
+function onCellClicked(elCell, i, j) {
+  if (!gGame.isOn || gBoard[i][j].isMarked) return;
+  const currCell = gBoard[i][j];
+  if (currCell.isMine) {
+    console.log('boom');
+    renderCell(i, j, MINE);
+    checkGameOver();
+  } else if (currCell.minesAroundCount === 0) {
+    //this cell has no mine as neighbors
+    elCell.classList.add('revealed'); //no-mines-around
+    currCell.isShown = true;
+    gGame.shownCount++;
+    renderCell(i, j, '0');
+    expandShown(elCell, i, j);
+  } else {
+    // this cell has mines as neighbors
+    currCell.isShown = true;
+    gGame.shownCount++;
+    elCell.classList.add('revealed');
+    renderCell(i, j, `${currCell.minesAroundCount}`);
+  }
+}
+
+function onCellMarked(elCell, i, j) {
+  const currCell = gBoard[i][j];
+  if (!gGame.isOn || currCell.isShown) return;
+  if (currCell.isMarked) {
+    currCell.isMarked = false;
+    renderCell(i, j, EMPTY);
+  } else {
+    currCell.isMarked = true;
+    renderCell(i, j, FLAG);
+  }
+}
+
+function checkGameOver() {
+  // Game ends when all mines are marked,
+  //  and all the other cells are shown
+}
+
+function expandShown(elCell, rowIdx, colIdx) {
+  // When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
+
+  // NOTE: start with a basic implementation that only opens the non-mine 1 st degree neighbors
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= gBoard.length) continue;
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= gBoard[0].length) continue;
+      if (i === rowIdx && j === colIdx) continue;
+      var currCell = gBoard[i][j];
+      const elCurrCell = document.querySelector(`.cell-${i}-${j}`);
+      if (currCell.isShown || currCell.isMarked) {
+        continue;
+      }
+      if (currCell.isMine) {
+        continue;
+      } else if (currCell.minesAroundCount === 0) {
+        elCurrCell.classList.add('revealed'); //no-mines-around'
+        currCell.isShown = true;
+        gBoard.shownCount++;
+        renderCell(i, j, '0');
+      } else {
+        currCell.isShown = true;
+        elCurrCell.classList.add('revealed');
+        gBoard.shownCount++;
+        renderCell(i, j, `${currCell.minesAroundCount}`);
+      }
+    }
+  }
+}
+
+function handleMineClicked() {}
+function onChangeLevel(level) {
+  const levelMap = {
+    beginner: { SIZE: 4, MINES: 2 },
+    medium: { SIZE: 8, MINES: 14 },
+    expert: { SIZE: 12, MINES: 32 },
+  };
+  gLevel = levelMap[level];
+  onInit();
+}
---
+++ js/utils.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function createMat(ROWS, COLS) {
+  const mat = [];
+  for (var i = 0; i < ROWS; i++) {
+    const row = [];
+    for (var j = 0; j < COLS; j++) {
+      row.push('');
+    }
+    mat.push(row);
+  }
+  return mat;
+}
+function preventRightClick() {
+  const elBoardContainer = document.querySelector('.board-container');
+  elBoardContainer.addEventListener('contextmenu', (e) => e.preventDefault());
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+function getRandColor() {
+  var color = '#';
+  var digits = '0123456789ABCDEF';
+  for (var i = 0; i < 6; i++) {
+    var randomDigit = Math.floor(Math.random() * 16);
+    color += digits[randomDigit];
+  }
+  return color;
+}
+
+//just fit to project
+function getEmptyLocation(board) {
+  var emptyLocations = [];
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      if (board[i][j] === EMPTY) {
+        emptyLocations.push({ i, j });
+      }
+    }
+  }
+  if (!emptyLocations.length) return null;
+  var randIdx = getRandomIntInclusive(0, emptyLocations.length - 1);
+  return emptyLocations[randIdx];
+}
